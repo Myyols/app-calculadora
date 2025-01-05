1bbc52c70dc267dc23c7edc79199906602441da2
myllene.lnortovz.santos <myllene.lnortovz.santos@escola.pr.gov.br>
2025-01-05 01:23:30 +0000

    first commit

---
+++ lib/calculadora.dart
@@ -0,0 +1,104 @@
+import 'package:expressions/expressions.dart';
+import 'package:flutter/material.dart';
+
+class Calculadora extends StatefulWidget {
+  const Calculadora({super.key});
+
+  @override
+  State<Calculadora> createState() => _CalculadoraState();
+}
+
+class _CalculadoraState extends State<Calculadora> {
+  final String _limpar = 'Limpar';
+  String _expressao = '';
+  String _resultado = '';
+
+  void _pressionarBotao(String valor) {
+    setState(() {
+      if (valor == _limpar) {
+        _expressao = '';
+        _resultado = '';
+      } else if (valor == '=') {
+        _calcularResultado();
+      } else {
+        _expressao += valor;
+      }
+    });
+  }
+
+  void _calcularResultado() {
+    setState(() {
+      _resultado = avaliarExpressao(_expressao).toString(); 
+    });
+  }
+
+  double avaliarExpressao(String expressao) {
+    // avaliar expressao com a biblioteca expressions
+    ExpressionEvaluator avaliador = const ExpressionEvaluator();
+    double resultado = avaliador.eval(Expression.parse(expressao), {});
+    return resultado;
+  }
+
+  Widget _botao(String valor) {
+    return ElevatedButton(
+      onPressed: () => _pressionarBotao(valor),
+      child: Text(valor),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Calculadora'),
+      ),
+      body: Column(
+        children: [
+          Expanded(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                Text(
+                  _expressao,
+                  style: const TextStyle(fontSize: 24),
+                ),
+                Text(
+                  _resultado,
+                  style: const TextStyle(fontSize: 24),
+                ),
+              ],
+            ),
+          ),
+          Expanded(
+            flex: 3,
+            child: GridView.count(
+              crossAxisCount: 4,
+              childAspectRatio: 2,
+              children: [
+                _botao('7'),
+                _botao('8'),
+                _botao('9'),
+                _botao('/'),
+                _botao('4'),
+                _botao('5'),
+                _botao('6'),
+                _botao('*'),
+                _botao('1'),
+                _botao('2'),
+                _botao('3'),
+                _botao('-'),
+                _botao('0'),
+                _botao('.'),
+                _botao('='),
+                _botao('+'),
+              ],
+            ),
+          ),
+          Expanded(
+            child: _botao(_limpar),
+          )
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,106 @@
+import 'package:flutter/material.dart';
+import 'calculadora.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Layout',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: const Color.fromARGB(255, 73, 183, 58)),
+        useMaterial3: true,
+      ),
+      home: const HomePage(title: 'Calculadora da my '),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({
+    super.key,
+    required this.title,
+  });
+
+  final String title;
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Expanded(
+              child: Container()
+            ), // Expanded
+            Expanded(
+              flex: 7,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Expanded(
+                    child: Container(
+                      // alignment: Alignment.center,
+                      // color: Colors.red,
+                      // child: const Text('Primeira coluna'),
+                    ),
+                  ),
+                  Expanded(
+                    flex: 2,
+                    child: Container(
+                      alignment: Alignment.center,
+                      decoration: BoxDecoration(
+                        color: Colors.blue,
+                        borderRadius: BorderRadius.circular(20),
+                        border: Border.all(
+                          color: Colors.black,
+                          width: 1,
+                        ),
+                        boxShadow: const [
+                          BoxShadow(
+                            color: Colors.black,
+                            blurRadius: 10,
+                            offset: Offset(5, 5),
+                          )
+                        ],
+                      ),
+                      child: const Calculadora(),
+                    ),
+                  ),
+                  Expanded(
+                    child: Container()
+                  ),
+                ],
+              ),
+            ),
+            Expanded(
+              child: Container()
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
